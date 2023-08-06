ded6b00d385fd2f5ade5298723814c536abb88b1
Sergey <sergio@Sergios-iMac.local>
2023-08-06 16:47:23 -0500

    add put and delete

--- app.js
+++ app.js
@@ -45,6 +45,34 @@ app.put('/api/people/:id', (req, res) => {
   const { id } = req.params
   const { name } = req.body
   console.log(id, name)
+  const person = people.find((person) => person.id === Number(id))
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id${id}` })
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name
+    }
+    return person
+  })
+  res.status(200).json({ success: true, data: newPeople })
+})
+
+app.delete('/api/people/:id', (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id))
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${req.params.id}` })
+  }
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id)
+  )
+  res.status(200).json({ success: true, data: newPeople })
 })
 
 app.listen(5000, () => {
