b113e9079ab3443d753389a823beef34ad196a83
Sergey <sergio@Sergios-iMac.local>
2023-08-13 13:52:53 -0500

    add storeAPI template

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const connectDB = require('./db/connectDB')
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
@@ -22,6 +23,7 @@ const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, console.log(`Server is listening ${port}`))
   } catch (error) {}
 }
