457d9187f2d8745ed4a3fe247f751861600722c9
Sherlok <sergei_malin@yahoo.com>
2023-08-17 19:30:56 -0500

    add routes

--- app.js
+++ app.js
@@ -4,14 +4,20 @@ require('express-async-errors')
 const express = require('express')
 const app = express()
 
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
+
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
+const { register } = require('./controllers/auth')
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('jobs api')
-})
+// app.get('/', (req, res) => {
+//   res.send('jobs api')
+// })
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
