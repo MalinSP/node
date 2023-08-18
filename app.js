49a056f8a49ab0234daa678903337da06c685dc9
Sherlok <sergei_malin@yahoo.com>
2023-08-17 19:35:26 -0500

    add db connection

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ require('express-async-errors')
 const express = require('express')
 const app = express()
 
+const connectDB = require('./db/connect')
+
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 
@@ -26,6 +28,7 @@ const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () => console.log(`Server is listening on ${port}`))
   } catch (error) {
     console.log(error)
