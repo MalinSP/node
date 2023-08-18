01ea875a15145a3c2ddb2e284e55141f0666991e
Sherlok <sergei_malin@yahoo.com>
2023-08-18 11:40:42 -0500

    generate token controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,14 @@
 const User = require('../models/user')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError } = require('../errors')
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
-  // const { name, email, password } = req.body
-  // if (!name || !email || !password) {
-  //   throw new BadRequestError('Please provide name, email and password')
-  // }
-  // const { name, email, password } = req.body
-
-  // const salt = await bcrypt.genSalt(10)
-  // const hashPassword = await bcrypt.hash(password, salt)
-  // const tempUser = { name, email, password: hashPassword }
   const user = await User.create({ ...req.body })
-  res.status(StatusCodes.CREATED).json({ user })
+  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', {
+    expiresIn: '30d',
+  })
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
 const login = async (req, res) => {
