17811d1bea9405aa0a22a1a9ed492ce406b063ee
Sherlok <sergei_malin@yahoo.com>
2023-08-19 11:10:54 -0500

    add deleteJob

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -47,7 +47,21 @@ const updateJob = async (req, res) => {
   res.status(StatusCodes.OK).json({ job })
 }
 const deleteJob = async (req, res) => {
-  res.send('delete Job')
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req
+
+  const job = await Job.findOneAndRemove({
+    _id: jobId,
+    createdBy: userId,
+  })
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+
+  res.status(StatusCodes.OK).send()
 }
 
 module.exports = {
