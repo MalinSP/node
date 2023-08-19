650b7326699ed8798d23ffcbb8bbab2eff246337
Sherlok <sergei_malin@yahoo.com>
2023-08-19 16:28:10 -0500

    security packages added

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 require('dotenv').config()
 require('express-async-errors')
 
+// security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express')
 const app = express()
 
@@ -14,7 +20,17 @@ const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 const { register } = require('./controllers/auth')
 
+app.set('trust proxy', 1)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    max: 100,
+  })
+)
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // app.get('/', (req, res) => {
 //   res.send('jobs api')
