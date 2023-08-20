09de16bd5398cd2169ae8f3b152629a7332ff5e7
Sherlok <sergei_malin@yahoo.com>
2023-08-20 16:33:53 -0500

    jobster api - setup frontend

---
+++ middleware/authentication.js
@@ -0,0 +1,26 @@
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const { UnauthenticatedError } = require('../errors')
+
+const authMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new UnauthenticatedError('Authentication invalid')
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET)
+
+    // const user = User.findById(payload.id).select("-password")
+    // req.user = user
+    req.user = { userId: payload.userId, name: payload.name }
+    next()
+  } catch (error) {
+    throw new UnauthenticatedError('Authentication invalid')
+  }
+}
+
+module.exports = authMiddleware
